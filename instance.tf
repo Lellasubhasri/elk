38244fb378c2d48d2e301d56f395bf6cbe344756
lella subhasri <subhasri.lella@cloudreach.com>
2022-08-25 16:22:00 +0530

    files added

--- instance.tf
+++ instance.tf
@@ -9,38 +9,8 @@ vpc_security_group_ids = [aws_security_group.elk_server.id]
   tags = {
     Name = "elk"
   }
-#   provisioner "file" {
-#     source = "kibana.yml"
-#     destination = "/tmp/kibana.yml"
-#     }
-#     provisioner "file" {
-#         source = "apache.conf"
-#         destination = "/tmp/apache.conf"
-      
-#     }
-
-    # provisioner "file" {
-    #   source = "code.sh"
-    #   destination = "/tmp/code.sh"
-    # }
-    # provisioner "file" {
-    #     inline =[
-    #         "chmod +x /tmp/code.sh",
-    #         "sudo /tmp/code.sh"]
-    #     }
-      
-    #   connection {
-    #     host = coalesce(self.public_ip, self.Private_ip)
-    #     type = "ssh"
-    #     #user = var.Instance_username
-    #     #private_key = file(var.path_to_private_key)
-    #   }
-    }
-    
-#     resource "aws_eip" "ansible_server_ip" {
-#     instance = aws_instance.ansible_server.id
-#     vpc      = true
-# }
+#  
+}
 
 
 
